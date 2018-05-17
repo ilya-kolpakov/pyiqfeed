d0226aa1914b8a79c635d6d5f66ee17d60383e97
Ilya Kolpakov <ilya.kolpakov@gmail.com>
2018-05-17 17:37:09 +0200

    minor fixes in quote_tracker

--- pyiqfeed/extra/quote_tracker.py
+++ pyiqfeed/extra/quote_tracker.py
@@ -14,13 +14,13 @@ class QuoteTracker(SilentQuoteListener):
 
     "Quote tracker runnning in background thread"
 
-    def __init__(self, symbols, mode='trades', port=9000, fieldnames=None):
+    def __init__(self, symbols, mode='trades', port=9000, fields=None):
         super().__init__('QuoteTracker')
         assert mode in ['trades', 'quotes']
         self._symbols = symbols
         self.mode = mode
         self.port = port
-        self.fieldnames = None
+        self.requested_fields = fields
         self.fundamentals = dict()
         self.last_update = dict()
         self.errors = list()
@@ -65,7 +65,7 @@ class QuoteTracker(SilentQuoteListener):
         self.process_update(summaries)
 
     def process_fundamentals(self, fundamentals):
-        for f in np.copy(fundamentals):
+        for f in fundamentals:
             decode = lambda s: s.decode('ascii') if isinstance(s, np.bytes_) else s
             d = dict(zip(map(decode, f.dtype.names), (decode(val) for val in f)))
             symbol = d.pop('Symbol')
@@ -73,7 +73,7 @@ class QuoteTracker(SilentQuoteListener):
             self.good_symbols.append(symbol)
  
     def process_update(self, updates: np.array):
-        for update in np.copy(updates):
+        for update in updates:
             symbol = update[0].decode('utf-8')
             self.last_update[symbol] = np.copy(update)
 
@@ -82,8 +82,8 @@ class QuoteTracker(SilentQuoteListener):
         conn.add_listener(self)
         self._conn = conn
         with ConnConnector([conn]) as connector:
-            if self.fieldnames:
-                conn.select_update_fieldnames(self.fieldnames)
+            if self.requested_fields:
+                conn.select_update_fieldnames(self.requested_fields)
             watch = conn.watch if self.mode == 'quotes' else conn.trades_watch
             for t in self._symbols:
                 watch(t)
@@ -132,18 +132,19 @@ if __name__ == '__main__':
         return [root] + [sep.join([root, s]) for s in sfx]
 
     roots_with_suffixes = {
-        'Last': ('Date', 'Time', 'Size'),
-        'Most Recent Trade': ('Date', 'Time', 'Size', 'Conditions'),
-        'Bid': ('Time', 'Size'),
-        'Ask': ('Time', 'Size')
+        'Last':                 ('Date', 'Time', 'Size'),
+        'Bid':                  ('Time',         'Size'),
+        'Ask':                  ('Time',         'Size'),
+        'Most Recent Trade':    ('Date', 'Time', 'Size', 'Conditions'),
     }
 
-    default_fieldnames = []
+    default_fields = []
     for root, sfx in roots_with_suffixes.items():
-        default_fieldnames += with_suffixes(root, sfx)
+        default_fields += with_suffixes(root, sfx)
 
     tickers = open(sys.argv[1]).read().strip().split(' ')
-    qt = QuoteTracker(tickers, 'quotes')
+    print('Loaded %d tickers from %s. Connecting...' % (len(tickers), sys.argv[1]))
+    qt = QuoteTracker(tickers, 'quotes', fields=default_fields)
     qt.start()
 
     dt = 0.5
@@ -157,7 +158,11 @@ if __name__ == '__main__':
         print('Could not connect within %d seconds. Exiting...' % t)
         sys.exit(-1)
 
-    print('Symbols found: %s', ' '.join(qt.good_symbols))
-    print('Symbols not found: %s', ' '.join(qt.bad_symbols))
+    time.sleep(2)
+    print('%d symbols found.' % len(qt.good_symbols))
+
+    from textwrap import TextWrapper
+    tw = TextWrapper(subsequent_indent=' '*4)
+    print(tw.fill('Symbols not found: %s' % ' '.join(qt.bad_symbols)))
 
     print('Last quote from AAPL is %s', str(qt['AAPL']))
